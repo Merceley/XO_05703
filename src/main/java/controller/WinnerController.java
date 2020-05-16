1f94f3b7773e5b9dd16912637574ba4901134b56
b0mbarnac <hominizlande@gmail.com>
2020-05-16 12:40:37 +0300

    WinnerController - прорисовка линии для победы по диагонали.

--- src/main/java/controller/WinnerController.java
+++ src/main/java/controller/WinnerController.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.util.HashMap;
 
 public class WinnerController {
-    int[] arr;
     private  int[][] winLines1;
     public static int[][] winLine;
     private  int[][] winLines2;
